88524113c97a9b26b67b2fdb998587a6f726a62c
Moshe Dahan <38052895+dikodahan@users.noreply.github.com>
2019-03-15 16:17:24 -0400

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -34,44 +34,6 @@ BINDIR="$PIHOMEDIR/$GITLABPROJECT"
 USER="pi"
 
 #####################################################################
-# Functions
-execute() { #STRING
-  if [ $# != 1 ] ; then
-    echo "ERROR: No args passed"
-    exit 1
-  fi
-  cmd=$1
-
-  echo "[*] EXECUTE: [$cmd]"
-  eval "$cmd"
-  ret=$?
-
-  if [ $ret != 0 ] ; then
-    echo "ERROR: Command exited with [$ret]"
-    exit 1
-  fi
-
-  return 0
-}
-
-exists() { #FILE
-  if [ $# != 1 ] ; then
-    echo "ERROR: No args passed"
-    exit 1
-  fi
-
-  file=$1
-
-  if [ -f $file ]; then
-    echo "[i] FILE: [$file] exists."
-    return 0
-  else
-    echo "[i] FILE: [$file] does not exist."
-    return 1
-  fi
-}
-
-#####################################################################
 # LOGIC!
 echo "INSTALLING.."
 
@@ -81,82 +43,51 @@ if ! exists "$BINDIR/LICENSE" ; then
 fi
 execute "chown -R $USER:$USER $BINDIR"
 
-if ! exists "$DESTBOOT/config_ORIGINAL.txt" ; then
-    echo "Copying optimized config.txt:"
-    execute "cp $DESTBOOT/config.txt $DESTBOOT/config_ORIGINAL.txt"
-    execute "cp $BINDIR/settings/config.txt $DESTBOOT/config.txt"
-fi
-
-# Copy splashscreens
-if ! exists "$PIHOMEDIR/RetroPie/splashscreens/GBZ-Splash-Screen.mp4" ; then
-    echo "Copying splashscreen:"
-    execute "cp $BINDIR/splashscreens/GBZ-Splash-Screen.mp4 $PIHOMEDIR/RetroPie/splashscreens/GBZ-Splash-Screen.mp4"
-    if exists "$DEST/etc/splashscreen.list" ; then
-      echo "Setting default splashscreen default:"
-      execute "sed -i 's|$DEST/opt/retropie/supplementary/splashscreen/retropie-default.png|$PIHOMEDIR/RetroPie/splashscreens/GBZ-Splash-Screen.mp4|' $DEST/etc/splashscreen.list"
+# Set up Battery Indicator script and icon in RetroPie
+if ! exists "$PIHOMEDIR/RetroPie/retromenu/battery.sh" ; then
+    echo "Copying battery indicator script:"
+    execute "cp $BINDIR/scripts/battery.sh $PIHOMEDIR/RetroPie/retropiemenu"
+    echo "Copying battery icon:"
+    execute "cp $BINDIR/images/battery.png $PIHOMEDIR/RetroPie/retropiemenu/icons"
+    if grep -Fxq "battery.sh" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml; then
+      echo "Adding battery indicator menu item to RetroPie:"
+      batterysection=$(
+            cat <<_END_
+    <game>
+	    <path>./battery.sh</path>
+	    <name>Battery Indicator</name>
+	    <desc>Turns the battery indicator ON and OFF in Kite's HUD software.</desc>
+	    <image>./icons/battery.png</image>
+    </game>
+_END_
+        )
+      execute "sed -i "/\<\/gameList\>\n/a ${batterysection}" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml"
     fi
 fi
 
-# Fix splashsreen sound
-if exists "$DEST/etc/init.d/asplashscreen" ; then
-    execute "sed -i \"s/ *both/ alsa/\" $DEST/etc/init.d/asplashscreen"
+# Set up Kite's HUD Update script and icon in RetroPie
+if ! exists "$PIHOMEDIR/RetroPie/retromenu/kite-update.sh" ; then
+    echo "Copying HUD Update script:"
+    execute "cp $BINDIR/scripts/kite-update.sh $PIHOMEDIR/RetroPie/retropiemenu"
+    echo "Copying HUD Update icon:"
+    execute "cp $BINDIR/images/kite.png $PIHOMEDIR/RetroPie/retropiemenu/icons"
+    if grep -Fxq "kite-update.sh" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml; then
+      echo "Adding Kite's HUD update menu item to RetroPie:"
+      hudupdatesection=$(
+            cat <<_END_
+    <game>
+        <path>./kite-update.sh</path>
+        <name>Kite HUD Update</name>
+        <desc>Installs available updates to Kite's HUD software.</desc>
+        <image>./icons/kite.png</image>
+    </game>
+_END_
+        )
+      execute "sed -i "/\<\/gameList\>\n/a ${hudupdatesection}" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml"
+    fi
 fi
-if exists "$DEST/opt/retropie/supplementary/splashscreen/asplashscreen.sh" ; then
-    execute "sed -i \"s/ *both/ alsa/\" $DEST/opt/retropie/supplementary/splashscreen/asplashscreen.sh"
-fi
-
-# Install the gbz35 theme and set it as default
-if ! exists "$DEST/etc/emulationstation/themes/gbz35/gbz35.xml" ; then
-    echo "Installing theme for 3.5\" screens:"
-    execute "mkdir -p $DEST/etc/emulationstation/themes"
-    execute "rm -rf $DEST/etc/emulationstation/themes/gbz35"
-    execute "git clone --recursive --depth 1 --branch master https://github.com/rxbrad/es-theme-gbz35.git $DEST/etc/emulationstation/themes/gbz35"
-    execute "cp $BINDIR/settings/es_settings.cfg $DEST/opt/retropie/configs/all/emulationstation/es_settings.cfg"
-    execute "sed -i \"s/carbon/gbz35/\" $DEST/opt/retropie/configs/all/emulationstation/es_settings.cfg"
-    execute "chown $USER:$USER $DEST/opt/retropie/configs/all/emulationstation/es_settings.cfg"
-fi
-
-# Install the gbz35-dark theme
-if ! exists "$DEST/etc/emulationstation/themes/gbz35-dark/gbz35.xml" ; then
-    execute "mkdir -p $DEST/etc/emulationstation/themes"
-    execute "rm -rf $DEST/etc/emulationstation/themes/gbz35-dark"
-    execute "git clone --recursive --depth 1 --branch master https://github.com/rxbrad/es-theme-gbz35-dark.git $DEST/etc/emulationstation/themes/gbz35-dark"
-fi
-
-# Disable fsck
-# https://www.sudomod.com/forum/viewtopic.php?f=44&t=6292#p63567
-if exists "$DEST/opt/retropie/supplementary/splashscreen/asplashscreen.sh" ; then
-    echo "Disabling fsck on boot:"
-    execute "sed -i 's|defaults          0       2|defaults          0       0|' $DEST/etc/.fstab"
-    execute "sed -i 's|defaults,noatime  0       1|defaults,noatime  0       0|' $DEST/etc/.fstab"
-    execute "sed -i 's|defaults          0       2|defaults          0       0|' $DEST/etc/fstab"
-    execute "sed -i 's|defaults,noatime  0       1|defaults,noatime  0       0|' $DEST/etc/fstab"
-fi
-
-# Disable 'wait for network' on boot
-echo "Disabling 'wait for network' on boot:"
-execute "rm -f $DEST/etc/systemd/system/dhcpcd.service.d/wait.conf"
-
-# Disable fast forward when Select is pressed
-execute "echo 'input_toggle_fast_forward_btn = nul' >> $DEST/opt/retropie/configs/all/retroarch.cfg"
-
-# Change to home directory
-execute "cd $PIHOMEDIR"
-
-# Install Retrogame
-execute "curl https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/retrogame.sh >retrogame.sh"
-execute "bash retrogame.sh"
-execute "cp $BINDIR/settings/retrogame.cfg $DESTBOOT/retrogame.cfg"
-
-# Install battery monitor
-execute "wget https://gitlab.com/sixteenbitsystems/gbz-combo-script/raw/master/InstallComboScript.sh"
-execute "git clone https://gitlab.com/sixteenbitsystems/gbz-combo-script.git"
-execute "chmod +x InstallComboScript.sh"
-execute "./InstallComboScript.sh"
-
-# Install i2s script
-execute "curl -sS https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/i2samp.sh | bash"
 
 #####################################################################
 # DONE
 echo "DONE!"
+echo "You need to reboot your device in order for these changes to take effect."
