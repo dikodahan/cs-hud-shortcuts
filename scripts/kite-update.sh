cbdf0d8b7ba0c804c5510825249908b37db2e988
Moshe Dahan <moshed@Moshes-MBP-10.office.wt.us.infinidat.com>
2019-03-18 12:37:54 -0400

    Added a check to validate CS-HUD software is installed

--- scripts/kite-update.sh
+++ scripts/kite-update.sh
@@ -9,4 +9,8 @@ elif [[ -d "${HOME}/Circuit-Sword" ]]; then
 elif [[ -d "${HOME}/Circuit-Sword-Lite" ]]; then
   cd ~/Circuit-Sword-Lite
   sudo ./update.sh
+else
+  echo "It seems like Kite's CS-HUD software is not installed on your device :("
+  echo "Please follow Kite's guides and images to make sure it is installed: https://github.com/kiteretro"
+  sleep 10s
 fi
